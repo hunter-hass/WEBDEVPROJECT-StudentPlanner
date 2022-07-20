44a171b8528b30324efa0710e61592d34bc70a3b
hunter-hass <71414506+hunter-hass@users.noreply.github.com>
2022-07-20 19:07:01 -0400

    Update db_credentials.php

--- db_credentials.php
+++ db_credentials.php
@@ -5,7 +5,5 @@
 // (see 6.2 in the zyBook for refresher on constants)
 define("DB_SERVER", "localhost"); // Always connect to localhost (do not change this constant)
 define("DB_NAME", "hassh70"); // Because the name of your database is your username
-define("DB_USER", "hassh70" );
-define("DB_PWD", "mysql831170");
 
 ?>
